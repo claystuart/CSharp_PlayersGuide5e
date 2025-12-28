c832ad4cb5c59a27cdf5ffa08b13ef7d4f38f7d6
claystuart <clay.stuart@gmail.com>
2025-12-28 05:22:30 -0500

    Implement Level 6 alternative - Clay's Converter

---
+++ Lvl6_ClaysConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿// Description: Read age and convert string->int  // pages 47-48
+// Demonstrate different string->int conversion methods
+// Program won't convert sring->int if given a float value
+
+Console.Write("Enter age: ");
+string input = Console.ReadLine()!;
+
+/** Multiple Conversion methods **/
+//int age = Convert.ToInt32(input);
+//int age = int.Parse(input);
+bool success = int.TryParse(input, out int age); // TryParse described on page 275 (my preferred method)
+
+if (success)
+    Console.WriteLine($"Next year, you will be {++age} years old");
+else
+    Console.WriteLine("Error on input");
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ A Walkthrough of the "C# Player's Guide, Fifth Edition" by RB Whitaker.
 ### Level 4: Comments
 1. The Thing Namer 3000 - Comments and code cleanup
 ### Level 5: Basic variable usage - n/a
+### Level 6: C# Types - substitute project (convert string -> int)
+1. Clay's Converter
