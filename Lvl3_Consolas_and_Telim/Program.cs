a664700605b4e0252a5232ec0298e0521120c138
claystuart <clay.stuart@gmail.com>
2025-12-28 04:49:10 -0500

    Complete through Level 3

---
+++ Lvl3_Consolas_and_Telim/Program.cs
@@ -0,0 +1,6 @@
+﻿// Description: Get user's name and give them bread
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine()!; //"!" suppresses null warning
+Console.WriteLine($"Noted: {name} got bread");
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # C#PlayersGuide5e
+A Walkthrough of the "C# Player's Guide, Fifth Edition" by RB Whitaker.
+
+### Level 1 - Intro to .Net
+### Level 2 - Intro to Visual Studio
+### Level 3 - Compiling "Hello World"
+1. Consolas and Telim - Basic I/O
+### Level 4
