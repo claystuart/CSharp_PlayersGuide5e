a35af6107fe4d8ae979a42f9635eafe9efe14133
claystuart <clay.stuart@gmail.com>
2025-12-29 21:08:42 -0500

    Implement Level 11 Challenges: Prototype (Hi-Lo) and MagicCannon (Fizz-Buzz)

--- Lvl11_Loops/Lvl11_Loops.cs
+++ Lvl11_Loops/Lvl11_Loops.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Description: Implement "The Prototype" (p89): A Hi-Lo game
+// Description: Implement "The Magic Cannon" (p89): A Fizz-Buzz variant
+
+//Prototype();
+MagicCannon();
+
+// Hi-Lo: 0-100
+static void Prototype()
+{
+    Random rand = new(); //generate random numbers (p248)
+    int answer = rand.Next(0, 101);
+    Console.WriteLine("Guess the answer between 0 and 100");
+
+    while (true)
+    {
+        Console.Write("Enter guess: ");
+        string input = Console.ReadLine()!;
+        if (int.TryParse(input, out int guess))
+        {
+            if (guess == answer)
+            {
+                Console.WriteLine($"You guessed the number: {answer}");
+                break; // break from while loop and end program
+            }
+            else if (guess > answer)
+                Console.WriteLine("Too High");
+            else if (guess < answer)
+                Console.WriteLine("Too Low");
+        }
+        else
+            Console.WriteLine("Bad guess");
+    }
+}
+
+// Fizz-Buzz (1-100)
+static void MagicCannon()
+{
+    for (int x = 1; x <= 100; x++)
+    {
+        Console.ForegroundColor = ConsoleColor.White; // Reset ConsoleColor to White
+        if (x % 15 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{x}: Electric and Fire");
+        }
+        else if (x % 3 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{x}: Fire");
+        }
+        else if (x % 5 == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{x}: Electric");
+        }
+        else
+        {
+            Console.WriteLine($"{x}: Normal");
+        }
+    }
+}
